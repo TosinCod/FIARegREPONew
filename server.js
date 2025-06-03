f52a8c6584eb0815b0af31e915f0ded400741223
Tosin Code <adeking12@gmail.com>
2025-06-02 20:38:13 -0700

    quick fix

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const fetch = (...args) =>
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const GOOGLE_SCRIPT_URL = process.env.GOOGLE_SCRIPT_URL;
-const GOOGLE_SCRIPT_URL2 = process.env.GOOGLE_SCRIPT_URL2;
+/* const GOOGLE_SCRIPT_URL = process.env.GOOGLE_SCRIPT_URL;
+const GOOGLE_SCRIPT_URL2 = process.env.GOOGLE_SCRIPT_URL2; */
 
 app.use(cors());
 app.use(express.json());
@@ -16,11 +16,14 @@ app.use(express.json());
 app.post("/api/register", async (req, res) => {
   console.log("Incoming /api/register:", req.body);
   try {
-    const response = await fetch(GOOGLE_SCRIPT_URL2, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(req.body),
-    });
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbzIpeG-rcoDHHXEK7ic6LfhHpKvS5cAOsdcZHeBgcruWS7pNTSPsRVvZMOISc_lFnF5_w/exec",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(req.body),
+      }
+    );
 
     const data = await response.json();
     res.json(data);
@@ -32,11 +35,14 @@ app.post("/api/register", async (req, res) => {
 
 app.post("/api/becomeExpert", async (req, res) => {
   try {
-    const response = await fetch(GOOGLE_SCRIPT_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(req.body),
-    });
+    const response = await fetch(
+      "ttps://script.google.com/macros/s/AKfycbz67esJy0syCJnHCI-6oAUVnTX5YJuIUMMy2hSHl_vmf2v5iPen8AzkeJagD5SlN7BV2Q/exec",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(req.body),
+      }
+    );
 
     const data = await response.json();
     res.json(data);
